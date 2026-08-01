c614fc1e0ac09f864d0cf4d4263b9054ecd40c5e
steve-max09 <william.leask@viacesi.fr>
2026-08-01 17:02:47 +0200

    test

--- js/main.js
+++ js/main.js
@@ -11,7 +11,19 @@ import { DifficultyLevels } from "./config/difficultyLevels.js";
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./service-worker.js");
+    navigator.serviceWorker.register("./service-worker.js").then((registration) => {
+      registration.update();
+
+      registration.addEventListener("updatefound", () => {
+        const newWorker = registration.installing;
+        newWorker.addEventListener("statechange", () => {
+          if (newWorker.state === "installed" && navigator.serviceWorker.controller) {
+            newWorker.postMessage({ type: "SKIP_WAITING" });
+            window.location.reload();
+          }
+        });
+      });
+    });
   });
 }
 
